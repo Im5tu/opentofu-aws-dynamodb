08e6604a493d6b632a5c60800777f13beda6137f
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2026-01-01 14:58:34 +0000

    Initial dynamodb module extraction

--- _variables.tf
+++ _variables.tf
@@ -1,7 +1,97 @@
-# TODO: Add your module variables here
+variable "table_name" {
+  type        = string
+  description = "Name of the DynamoDB table"
+}
+
+variable "billing_mode" {
+  type        = string
+  description = "Billing mode for the table (PROVISIONED or PAY_PER_REQUEST)"
+  default     = "PAY_PER_REQUEST"
+
+  validation {
+    condition     = contains(["PROVISIONED", "PAY_PER_REQUEST"], var.billing_mode)
+    error_message = "Billing mode must be either PROVISIONED or PAY_PER_REQUEST"
+  }
+}
+
+variable "hash_key" {
+  type        = string
+  description = "The attribute to use as the hash (partition) key"
+  default     = "PK"
+}
+
+variable "range_key" {
+  type        = string
+  description = "The attribute to use as the range (sort) key"
+  default     = "SK"
+}
+
+variable "stream_enabled" {
+  type        = bool
+  description = "Enable DynamoDB Streams"
+  default     = true
+}
+
+variable "stream_view_type" {
+  type        = string
+  description = "Stream view type (KEYS_ONLY, NEW_IMAGE, OLD_IMAGE, NEW_AND_OLD_IMAGES)"
+  default     = "NEW_AND_OLD_IMAGES"
+
+  validation {
+    condition     = contains(["KEYS_ONLY", "NEW_IMAGE", "OLD_IMAGE", "NEW_AND_OLD_IMAGES"], var.stream_view_type)
+    error_message = "Stream view type must be one of: KEYS_ONLY, NEW_IMAGE, OLD_IMAGE, NEW_AND_OLD_IMAGES"
+  }
+}
+
+variable "deletion_protection_enabled" {
+  type        = bool
+  description = "Enable deletion protection for the table"
+  default     = false
+}
+
+variable "point_in_time_recovery_enabled" {
+  type        = bool
+  description = "Enable point-in-time recovery"
+  default     = false
+}
+
+variable "ttl_enabled" {
+  type        = bool
+  description = "Enable TTL"
+  default     = true
+}
+
+variable "ttl_attribute_name" {
+  type        = string
+  description = "The name of the TTL attribute"
+  default     = "DynamoTimeToLive"
+}
+
+variable "kms_key_arn" {
+  type        = string
+  description = "ARN of the KMS key for encryption at rest (optional)"
+  default     = null
+}
+
+variable "global_secondary_indexes" {
+  type = list(object({
+    name            = string
+    hash_key        = string
+    range_key       = string
+    projection_type = string
+  }))
+  description = "List of global secondary indexes"
+  default     = []
+}
+
+variable "replica_regions" {
+  type        = list(string)
+  description = "List of regions to create replicas in for global tables"
+  default     = []
+}
 
 variable "tags" {
-  description = "Tags to apply to resources"
   type        = map(string)
+  description = "Tags to apply to the DynamoDB table"
   default     = {}
 }
--- main.tf
+++ main.tf
@@ -1,1 +1,75 @@
-# TODO: Add your module resources here
+locals {
+  # Collect all unique attribute names from primary keys and GSIs
+  all_attributes = toset(compact(concat(
+    [var.hash_key],
+    var.range_key != null && var.range_key != "" ? [var.range_key] : [],
+    [for gsi in var.global_secondary_indexes : gsi.hash_key],
+    [for gsi in var.global_secondary_indexes : gsi.range_key if gsi.range_key != null && gsi.range_key != ""]
+  )))
+}
+
+resource "aws_dynamodb_table" "this" {
+  name                        = var.table_name
+  billing_mode                = var.billing_mode
+  hash_key                    = var.hash_key
+  range_key                   = var.range_key
+  stream_enabled              = var.stream_enabled
+  stream_view_type            = var.stream_enabled ? var.stream_view_type : null
+  deletion_protection_enabled = var.deletion_protection_enabled
+
+  # All unique attributes (deduplicated from primary keys and GSIs)
+  dynamic "attribute" {
+    for_each = local.all_attributes
+    content {
+      name = attribute.value
+      type = "S"
+    }
+  }
+
+  # TTL configuration
+  ttl {
+    enabled        = var.ttl_enabled
+    attribute_name = var.ttl_attribute_name
+  }
+
+  # Point-in-time recovery
+  point_in_time_recovery {
+    enabled = var.point_in_time_recovery_enabled
+  }
+
+  # Global secondary indexes
+  dynamic "global_secondary_index" {
+    for_each = var.global_secondary_indexes
+    content {
+      name            = global_secondary_index.value.name
+      hash_key        = global_secondary_index.value.hash_key
+      range_key       = global_secondary_index.value.range_key
+      projection_type = global_secondary_index.value.projection_type
+    }
+  }
+
+  # Encryption
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = var.kms_key_arn
+  }
+
+  # Replicas for global tables
+  dynamic "replica" {
+    for_each = var.replica_regions
+    content {
+      region_name            = replica.value
+      kms_key_arn            = var.kms_key_arn
+      point_in_time_recovery = var.point_in_time_recovery_enabled
+    }
+  }
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [
+      read_capacity,
+      write_capacity
+    ]
+  }
+}
